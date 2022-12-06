2095d9e803e04074ace2cf5ab885015c785fe53c
Anyilo <anyilojoserangelarias@gmail.com>
2022-12-06 16:01:29 -0500

    carrito de compra

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 let menuEmail = document.querySelector(".navbar-email");
 let desktopMenu = document.querySelector(".desktop-menu");
 let burguerMenu = document.querySelector(".menu");
+let menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 let mobileMenu = document.querySelector(".mobile-menu");
+let aside = document.querySelector(".product-detail")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
-burguerMenu.addEventListener("click", toggleMobileMenu)
+burguerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside)
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        aside.classList.add("inactive");
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+    
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed= mobileMenu.classList.contains("inactive");
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive")
+    }
+    aside.classList.toggle("inactive");
 }
 
